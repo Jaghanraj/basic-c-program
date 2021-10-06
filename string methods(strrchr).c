b9717f74692cc47f88bfedcc0c77437abb27f6c9
Jaghanraj <89654268+Jaghanraj@users.noreply.github.com>
2021-10-06 16:30:21 +0530

    Create string methods(strrchr).c

---
+++ string methods(strrchr).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+ char mystr[50]="I'm an example for function strchr";
+ printf("%s",strrchr(mystr,'f'));
+   return 0;
+}
+
+
+output
+
+function strchr
