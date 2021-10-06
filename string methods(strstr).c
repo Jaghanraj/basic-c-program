b74f7719a01ca21fa1eb0351594fb7d8208e5e52
Jaghanraj <89654268+Jaghanraj@users.noreply.github.com>
2021-10-06 16:34:25 +0530

    Create string methods(strstr).c

---
+++ string methods(strstr).c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char inputstr[70]="String functions in c at beginers book.com";
+  printf("output string is :%s",strstr(inputstr,"begi"));
+   return 0;
+}
+
+output
+
+output string is :beginers book.com
