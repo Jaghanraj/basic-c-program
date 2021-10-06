54ad6cfa023d86e3a8dcc406ad7d44a08fc756e7
Jaghanraj <89654268+Jaghanraj@users.noreply.github.com>
2021-10-06 15:45:33 +0530

    Create String methods(strlen).c

---
+++ String methods(strlen).c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    char strl[20];
+    printf("Enter any string:");
+    gets(strl);
+    printf("The length of the string is:%d",strlen(strl));
+    return 0;
+}
+
+
+output
+
+Enter any string:hellocprogram
+The length of the string is:13
