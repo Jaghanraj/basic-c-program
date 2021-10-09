dddf5345bccb7268f131e5dbaa2aa3d1c6fc34af
Jaghanraj <89654268+Jaghanraj@users.noreply.github.com>
2021-10-09 15:55:48 +0530

    Create methods in c(strcmp).c

---
+++ methods in c(strcmp).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include<stdio.h>
+int
+main ()
+{
+  char str1[20];
+  char str2[20];
+  int value;
+  printf ("Enter the first string :");
+  scanf ("%s", str1);
+  printf ("Enter the second string :");
+  scanf ("%s", str2);
+  value = strcmp (str1, str2);
+  if (value == 0)
+    {
+      printf ("strings are same");
+    }
+  else
+    {
+      printf ("strings are not same");
+    }
+  return 0;
+}
+
+
+output
+
+Enter the first string :hello
+Enter the second string :c
+strings are not same
