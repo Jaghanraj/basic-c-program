0f8423b55c9b560ac042e6af292dcf65df7827fe
Jaghanraj <89654268+Jaghanraj@users.noreply.github.com>
2021-10-13 14:57:59 +0530

    Create Properties of loops exp1.c

---
+++ Properties of loops exp1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+ {
+ int a,b,c;
+ for(a=0,b=12,c=23;a<2;a++)
+ {
+     printf("%d\n",a+b+c);
+ }
+ return 0;
+}
+
+
+output
+
+#include <stdio.h>
+int main()
+ {
+ int a,b,c;
+ for(a=0,b=12,c=23;a<2;a++)
+ {
+     printf("%d\n",a+b+c);
+ }
+ return 0;
+}
+
+35
+36
